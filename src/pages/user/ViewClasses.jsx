38dc49d94869fddfc9c1adb09c8b318919063f26
mathan4 <109868924+mathan4@users.noreply.github.com>
2025-06-18 22:29:53 +0530

    Deployment issue fixed

--- src/pages/user/ViewClasses.jsx
+++ src/pages/user/ViewClasses.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import courseService from '../services/courseService';
+import courseService from '../../services/courseService';
 
 const ViewClasses = () => {
   const { courseId } = useParams();
